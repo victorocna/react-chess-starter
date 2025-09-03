e1b2274a96044453fb007bfcd8111c174dc228bf
victorocna <victorocnarescu@gmail.com>
2025-09-03 18:05:42 +0300

    Add more pages

---
+++ pages/pgn-viewer.js
@@ -0,0 +1,23 @@
+import { PgnFileLoader } from '@chess/components';
+import { PgnViewerLayout } from '@chess/components/PgnViewer';
+import { ChessProvider, PuzzleProvider } from '@chess/contexts';
+import { useLocalPgn } from '@chess/hooks';
+import { Layout } from '@components';
+
+const Page = () => {
+  const { key, fen, pgn, setPgn } = useLocalPgn();
+
+  return (
+    <Layout key={key} title="PGN viewer" button={<PgnFileLoader onPgnLoad={setPgn} />}>
+      <ChessProvider fen={fen}>
+        <PuzzleProvider>
+          <div className="grid md:grid-cols-2 gap-12 bg-white p-6 rounded-md border">
+            <PgnViewerLayout pgn={pgn} />
+          </div>
+        </PuzzleProvider>
+      </ChessProvider>
+    </Layout>
+  );
+};
+
+export default Page;
