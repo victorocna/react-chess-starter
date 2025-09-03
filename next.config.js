0224d294cc089daf98ee41812b9e89007456bdb1
victorocna <victorocnarescu@gmail.com>
2025-09-03 18:07:54 +0300

    Update env vars

--- next.config.js
+++ next.config.js
@@ -3,14 +3,7 @@ const headers = require('./headers');
 
 module.exports = {
   env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-    APP_BASE_URL: process.env.APP_BASE_URL,
     FONT_AWESOME_KEY: process.env.FONT_AWESOME_KEY,
-    JWT_TOKEN_NAME: process.env.JWT_TOKEN_NAME,
-    MAINTENANCE_MODE: process.env.MAINTENANCE_MODE,
-    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
-    SHOW_FORM_DEBUG: process.env.SHOW_FORM_DEBUG,
-    SHOW_LOGS_EVERYWHERE: process.env.SHOW_LOGS_EVERYWHERE,
   },
   eslint: {
     ignoreDuringBuilds: true,
