e9d1528e5f8f3dbd3123ac2b5247ef72f8ff022e
victorocna <victorocnarescu@gmail.com>
2025-09-05 12:36:21 +0300

    Add toaster on complete

--- pages/drill.js
+++ pages/drill.js
@@ -3,16 +3,20 @@ import { DrillLayout } from '@chess/components/Drill';
 import { ChessProvider, DrillProvider } from '@chess/contexts';
 import { useLocalPgn } from '@chess/hooks';
 import { Layout } from '@components';
+import { toaster } from '@lib';
 
 const Page = () => {
   const { key, fen, pgn, setPgn } = useLocalPgn();
+  const handleComplete = () => {
+    toaster.success('Drill completed successfully 🎉');
+  };
 
   return (
     <Layout key={key} title="Chess drills" button={<PgnFileLoader onPgnLoad={setPgn} />}>
       <ChessProvider fen={fen}>
         <DrillProvider mode="arrows">
           <div className="grid md:grid-cols-2 gap-12 bg-white p-6 rounded-md border">
-            <DrillLayout pgn={pgn} showMoves={true} />
+            <DrillLayout pgn={pgn} onComplete={handleComplete} showMoves={true} />
           </div>
         </DrillProvider>
       </ChessProvider>
--- pages/puzzle.js
+++ pages/puzzle.js
@@ -3,16 +3,20 @@ import { PuzzleLayout } from '@chess/components/Puzzle';
 import { ChessProvider, PuzzleProvider } from '@chess/contexts';
 import { useLocalPgn } from '@chess/hooks';
 import { Layout } from '@components';
+import { toaster } from '@lib';
 
 const Page = () => {
   const { key, fen, pgn, setPgn } = useLocalPgn();
+  const handleComplete = () => {
+    toaster.success('Puzzle completed successfully 🎉');
+  };
 
   return (
     <Layout key={key} title="Chess puzzles" button={<PgnFileLoader onPgnLoad={setPgn} />}>
       <ChessProvider fen={fen}>
         <PuzzleProvider>
           <div className="grid md:grid-cols-2 gap-12 bg-white p-6 rounded-md border">
-            <PuzzleLayout pgn={pgn} showMoves={true} />
+            <PuzzleLayout pgn={pgn} onComplete={handleComplete} showMoves={true} />
           </div>
         </PuzzleProvider>
       </ChessProvider>
