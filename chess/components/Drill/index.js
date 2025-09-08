eb0a3919724a73ab9b1e52adb694a958be7283f8
victorocna <victorocnarescu@gmail.com>
2025-09-08 15:39:08 +0300

    Move components

--- chess/components/Drill/index.js
+++ chess/components/Drill/index.js
@@ -1,2 +1,3 @@
 export { default as DrillBoard } from './DrillBoard';
 export { default as DrillLayout } from './DrillLayout';
+export { default as SwitchDrillMode } from './SwitchDrillMode';
--- chess/components/index.js
+++ chess/components/index.js
@@ -1,4 +1,3 @@
 export { default as FeedbackIcon } from './FeedbackIcon';
 export { default as GameSheet } from './GameSheet';
 export { default as PgnFileLoader } from './PgnFileLoader';
-export { default as SwitchDrillMode } from './SwitchDrillMode';
