27de33a99a975b21691bbb2dcfd2ac197332fe73
victorocna <victorocnarescu@gmail.com>
2025-09-08 21:29:33 +0300

    Add functions, hooks and pages

--- chess/hooks/index.js
+++ chess/hooks/index.js
@@ -1,6 +1,8 @@
 export { default as useEqualHeight } from './use-equal-height';
 export { default as useLocalPgn } from './use-local-pgn';
 export { default as useMainline } from './use-mainline';
+export { default as useMoveArrow } from './use-move-arrow';
+export { default as useMoveTrainer } from './use-move-trainer';
 export { default as usePgnViewer } from './use-pgn-viewer';
 export { default as useShapes } from './use-shapes';
 export { default as useVariationArrows } from './use-variation-arrows';
