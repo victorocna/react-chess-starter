8c7475413ab14974ddc9186864b9fa53be15b7b0
victorocna <victorocnarescu@gmail.com>
2025-09-30 10:27:55 +0300

    Add empty pgn

---
+++ chess/constants/empty-pgn.js
@@ -0,0 +1,7 @@
+// Empty PGN representing the starting position in chess
+export const emptyPgn = [
+  '[FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"]',
+  '[SetUp "1"]',
+  '',
+  '*',
+].join('\n');
