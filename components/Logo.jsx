29cab502021c9321efb063fe2af5185552f940c1
Bercaru Robert <robert.bercaru@chesscoders.com>
2026-06-05 15:16:37 +0300

    Update logo image styling and replace logo asset
    - Changed logo image styling to use responsive width and height.
    - Updated the logo image asset for improved quality.

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <div className="logo mb-6 flex cursor-pointer items-start">
-        <img src="/images/logo.png" className="w-28 h-28" alt={sitename} />
+        <img src="/images/logo.png" className="h-auto w-full" alt={sitename} />
       </div>
     </Link>
   );
