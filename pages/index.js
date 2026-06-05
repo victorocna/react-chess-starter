268d6ace386eac4ee6d52259210abe4d9b3115f5
Bercaru Robert <robert.bercaru@chesscoders.com>
2026-06-05 16:44:03 +0300

    Refactor PGN viewer by removing PgnNavigator component
    - Removed the `PgnNavigator` component from the main page to simplify the interface.
    - Retained the `PgnFileLoader` for loading PGN files and updated the layout accordingly.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
-import { PgnFileLoader, PgnNavigator } from '@chess/components';
+import { PgnFileLoader } from '@chess/components';
 import { useLocalPgn } from '@chess/hooks';
 import { Layout } from '@components';
 import NextChessground from 'next-chessground';
 
 const Page = () => {
-  const { key, fen, setPgn, games, currentGameIndex, setGameIndex } = useLocalPgn();
+  const { key, fen, setPgn } = useLocalPgn();
 
   return (
     <Layout key={key} title="Basic chess board" button={<PgnFileLoader onPgnLoad={setPgn} />}>
       <div className="grid md:grid-cols-2 gap-6 md:gap-12 bg-white p-6 rounded-md border">
         <NextChessground fen={fen} />
-        <div className="flex flex-col">
-          <PgnNavigator games={games} currentIndex={currentGameIndex} onGameSelect={setGameIndex} />
-        </div>
       </div>
     </Layout>
   );
